3eeee0ba948964ecf685484399a70053cfedccff
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-12-04 11:09:38 +0200

    message

---
+++ src/components/indicators_components/IndicatorUtils.jsx
@@ -0,0 +1,131 @@
+export const statuses = [
+    { label: 'Proposed', value: 'Proposed' },
+    { label: 'Draft', value: 'Draft' },
+    { label: 'Formally approved', value: 'Formally approved' },
+    { label: 'Piloting', value: 'Piloting' },
+    { label: 'keno', value: '' },
+    { label: 'Deployed', value: 'Deployed' },
+
+
+]; // Example list of options for dropdown
+
+
+export const domains = [
+    { label: 'Secundary Healthcare',value:'Secundary Healthcare' },
+    { label: 'Home/telecare and Active life', value: 'Home/telecare and Active life' },
+    { label: 'Primary Care', value: 'Primary Care' },
+    { label: 'Public health indicators', value:'Public health indicators' },
+    { label: 'NCDs and Mental Health',value:'NCDs and Mental Health' },
+    { label: 'Palliative and Long term care', value: 'Palliative and Long term care' },
+    { label: 'keno', value: '' },
+
+    { label: 'All domains', value: 'All domains' },
+];
+
+export const type_of_healthcare_providers_D1_D7list = [
+
+    { label: 'D1',value:'D1' },
+    { label: 'D2', value: 'D2' },
+    { label: 'D3', value: 'D3' },
+    { label: 'D4', value:'D4' },
+    { label: 'D5',value:'D5' },
+    { label: 'D6', value: 'D6' },
+    { label: 'keno', value: '' },
+    { label: 'D7', value: 'D7' }
+  
+
+]
+
+export const dimensions = [
+    { label: 'safety', value: 'safety' },
+    { label: 'keno', value: '' },
+
+    { label: 'effectiveness', value: 'effectiveness' }
+
+
+]; // Example list of options for dropdown
+
+export const classification_dimension = [
+    { label: 'A_Inputs_Workforce (Quantity/Training; capacity building)', value: 'A' },
+    { label: 'B_Inputs_Social determinants of health and care', value: 'B' },
+    { label: 'C_Process_Equity (gender balance, minorities, migrants)', value: 'C' },
+    { label: 'D_Process_Access', value: 'D' },
+    { label: 'H_Process_Patient Experience', value: 'H' },
+    { label: 'E_Process_Cost containing and efficiency', value: 'E' },
+    { label: 'F_Process_Green and sustainability', value: 'F' },
+    { label: 'G_Process_Digital and healthcare transformation', value: 'G' },
+    { label: 'H_Outputs_Patient Satisfaction', value: 'H' },
+    { label: 'I_Outputs_Health Gains', value: 'I' }
+];
+
+
+export const cross_Cutting_Dimensions_Inputs_Process_Outputlist = [
+     { label: 'Input', value: 'Input' },
+    { label: 'Process', value: 'Process' },
+    { label: 'Output', value: 'Outputs' },
+];
+
+export const category_of_indicators = [
+    { label: '', value: 'safety' },
+    { label: 'keno', value: '' },
+
+    { label: 'effectiveness', value: 'effectiveness' }
+
+
+]; // Example list of options for dropdown
+
+export const QoCOfficeReportlist = [
+    { label: 'Efficiency', value: 'Efficiency' },
+    { label: 'Access', value: 'Access' },
+    { label: 'Equity', value: 'Equity' },
+    { label: 'User experience', value: 'User experience' },
+    { label: 'Effectiveness', value: 'Effectiveness' },
+    { label: 'keno', value: '' },
+    { label: 'Safety', value: 'Safety' }
+];
+
+export const prioritylist = [
+    { label: '1st priority', value: '1' },
+    { label: '2nd priority', value: '2' },
+    { label: 'keno', value: '' },
+    { label: '3rd priority', value: '3' }
+];
+
+
+export const data_collection_list = [
+    { label: 'AUTOMATED', value: 'AUTOMATED' },
+    { label: 'MANUAL', value: 'MANUAL' },
+    { label: 'keno', value: '' },
+    { label: 'AUTOMATED NEW / INNOVATIVE', value: 'AUTOMATED NEW / INNOVATIVE' }
+];
+
+export const legal_Organizational_Requirements_list = [
+    { label: 'No need', value: '1' },
+    { label: 'Legal base exist but needs to be "operationalized" via a guideline/regulation by ODYPI/Other org', value: '2' },
+    { label: 'New Legal base is needed (MoH order)', value: '3' },
+    { label: 'keno', value: '' },
+    { label: 'New Legal base is needed (Law)', value: '4' }
+];
+
+export const selected_indicator_list = [
+    { label: 'YES', value: 'YES' },
+    { label: 'NO', value: 'NO' },
+    { label: 'keno', value: '' },
+
+    { label: 'MAYBE', value: 'MAYBE' }
+];
+
+
+export const piloting_list = [
+    { label: 'YES', value: 'YES' },
+    { label: 'NO', value: 'NO' },
+    { label: 'keno', value: '' },
+    { label: 'N/A', value: 'N/A' }
+];
+
+export const pilot_outcome_list = [
+    { label: 'MAINTAIN AS IS', value: '1' },
+    { label: 'MAINTAIN but adjust', value: '2' },
+    { label: 'keno', value: '' },
+    { label: 'SUSPEND', value: '3' }
+];
